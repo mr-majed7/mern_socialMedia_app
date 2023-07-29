f3e6c28cacf2fc65c625b40122cd4a7982b373c2
Majed <majedulislam7789@gmail.com>
2023-07-30 00:33:19 +0600

    fix authorization bug

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,7 +7,7 @@ export const getUser = async (req,res) => {
         const user = await User.findById(id);
         res.status(200).json(user);
     }catch(err){
-        res.status(500).json({error: err.message});
+        res.status(404).json({error: err.message});
     }
 }
 
