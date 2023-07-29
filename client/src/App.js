be4df29f3f882aca3c0562cd3c070f124b71c1ab
Majed <majedulislam7789@gmail.com>
2023-07-29 20:37:39 +0600

    add login and registration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from './scenes/homePage';
 import LoginPage from 'scenes/loginPage';
-import Navbar from 'scenes/navbar';
 import ProfilePage from 'scenes/profilePage';
 import { useMemo } from 'react';
 import { useSelector } from "react-redux";
@@ -12,6 +11,7 @@ import { themeSettings } from 'theme';
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
       <BrowserRouter>
@@ -19,8 +19,8 @@ function App() {
         <CssBaseline/>
         <Routes>
           <Route path="/" element={<LoginPage/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/profile/:userId" element={<ProfilePage/>}/>
+          <Route path="/home" element={isAuth ? <HomePage/>:<Navigate to ='/' />}/>
+          <Route path="/profile/:userId" element={isAuth ? <ProfilePage/>:<Navigate to = "/"/>}/>
 
         </Routes>
         </ThemeProvider>
